ac7400c2dda4fc327e43d7b8f9fa6acbb710f8fe
Duncan Kolba <duncankolba@gmail.com>
2017-08-15 19:33:40 +0000

    Use article slug as firebase key in import script

--- setup/import.js
+++ setup/import.js
@@ -1,47 +1,50 @@
-var fs = require('fs'),
+const fs = require('fs'),
   path = require('path'),
-  matter = require('gray-matter')
-  admin = require("firebase-admin");
+  matter = require('gray-matter'),
+  admin = require('firebase-admin');
 const dir = path.join(__dirname, 'import');
 const files = fs.readdirSync(dir);
 
 // Fetch the service account key JSON file contents
-const serviceAccount = require("../serviceAccountKey.json");
+const serviceAccount = require('../serviceAccountKey.json');
 
 const firebase = admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://nausika-5918e.firebaseio.com"
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://nausika-5918e.firebaseio.com',
 });
 
-var db = admin.database();
-var articlesRef = db.ref("articles");
+const db = admin.database();
+const articlesRef = db.ref('articles');
 
-const articles = { docs: [] };
 let promises = [];
 
-for (file of files) {
+for (let file of files) {
   const filename = file.split('.');
   const fileextension = filename[filename.length - 1];
 
   if (fileextension === 'md') {
-
     const doc = matter.read(path.join(dir, file));
-    const article = (docu) => {
+    const article = docu => {
       return Object.assign({}, docu.data, {
         content: docu.content,
-      })
+      });
     };
 
-    promises.push(articlesRef.push().set(article(doc), function(error) {
-      if (error) {
-        console.log("Data could not be saved." + error);
-      } else {
-        console.log("Data saved successfully.");
-      }
-    }));
+    promises.push(
+      articlesRef.child(doc.data.slug).set(article(doc), function(error) {
+        if (error) {
+          console.log('Data could not be saved.' + error);
+        } else {
+          console.log('Data saved successfully.');
+        }
+      })
+    );
   }
 }
 
 Promise.all(promises)
-  .then(() => {console.log('all done');firebase.delete()})
+  .then(() => {
+    console.log('all done');
+    firebase.delete();
+  })
   .catch(() => console.error('fark'));
